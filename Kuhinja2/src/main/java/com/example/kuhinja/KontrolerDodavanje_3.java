3361efd4ec50c21c041cbb450f12091e5245bb0e
MickyNM <140019715+MickyNM@users.noreply.github.com>
2023-07-29 20:38:25 +0200

    Update KontrolerDodavanje_3.java

--- Kuhinja2/src/main/java/com/example/kuhinja/KontrolerDodavanje_3.java
+++ Kuhinja2/src/main/java/com/example/kuhinja/KontrolerDodavanje_3.java
@@ -12,18 +12,13 @@ import java.util.Optional;
 public class KontrolerDodavanje_3 {
     @FXML
     private TextField nazivJelaField;
-    //    @FXML
-//    private TextField idKuhinjeField;
     @FXML
     private TextArea sastojciJelaArea;
     @FXML
     private TextField velicinaPorcijeField;
-//    private ObradaPodataka obradaPodataka = new ObradaPodataka();
-
     @FXML
     public ListaJela_2 ubaciJelo() {
         String naziv = nazivJelaField.getText().trim();
-//        int id_kuhinje = Integer.parseInt(idKuhinjeField.getText().trim());
         String sastojci = sastojciJelaArea.getText().trim();
         int velicina_porcije;
         if (velicinaPorcijeField.getText().trim().isEmpty()) {
